5e396482353c099e323dcef25c93b8daeee6d9d5
dkaslovsky <dkaslovsky@gmail.com>
2021-09-05 11:29:30 -0600

    cli

---
+++ cmd/cli.go
@@ -0,0 +1,141 @@
+package cmd
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"sort"
+	"strings"
+	"time"
+
+	"github.com/dkaslovsky/calendar-tasks/pkg/tasks"
+)
+
+const (
+	name            = "calendar-tasks"
+	printTimeFormat = "[Mon] Jan 2 2006"
+)
+
+type cmdArgs struct {
+	days             int
+	weeklySources    stringSliceArg
+	monthlySources   stringSliceArg
+	multiDateSources stringSliceArg
+}
+
+func (args *cmdArgs) parseArgs(argsIn []string) error {
+	fs := flag.NewFlagSet("calendar-tasks", flag.ExitOnError)
+	fs.IntVar(&args.days, "days", 0, "days ahead to get tasks")
+	fs.Var(&args.weeklySources, "weekly", "weekly task source file path")
+	fs.Var(&args.monthlySources, "monthly", "monthly task source file path")
+	fs.Var(&args.multiDateSources, "multi", "multiDate task source file path")
+
+	setUsage(fs)
+	return fs.Parse(argsIn)
+}
+
+// Run excutes the CLI
+func Run(argsIn []string) error {
+	args := &cmdArgs{}
+	err := parseArgs(args, argsIn)
+	if err != nil {
+		return err
+	}
+
+	date := fixDate(time.Now())
+
+	taskChan := make(chan tasks.Task, 1000) // buffer large enough for reasonable amount of tasks
+	done := make(chan struct{})
+
+	loader := tasks.NewLoader(taskChan, done)
+	processor := tasks.NewProcessor(date, args.days, taskChan, done)
+
+	loader.AddWeeklySource(args.weeklySources...)
+	loader.AddMonthlySource(args.monthlySources...)
+	loader.AddMultiDateSource(args.multiDateSources...)
+
+	err = start(loader, processor)
+	if err != nil {
+		return err
+	}
+
+	printTasks(processor, args.days, date)
+	return nil
+}
+
+func start(loader *tasks.Loader, processor *tasks.Processor) error {
+	// start the processor and wait on it to finish before returning
+	processor.Start()
+	defer processor.Wait()
+
+	// start the loader and return any errors
+	return loader.Start()
+}
+
+// fixDate returns a time.Time object matching the year, month, day (and location) of the argument
+// and sets the hour to the middle of the day to avoid any boundary cases that can occur with
+// e.g., daylight savings
+func fixDate(now time.Time) time.Time {
+	return time.Date(now.Year(), now.Month(), now.Day(), 12, 0, 0, 0, now.Location())
+}
+
+func printTasks(processor *tasks.Processor, numDays int, startDate time.Time) {
+	for day := 0; day <= numDays; day++ {
+		tsks, ok := processor.GetTasks(day)
+		if !ok {
+			continue
+		}
+
+		// sort for consistent ordering
+		sort.Slice(tsks, func(i, j int) bool {
+			return strings.ToLower(tsks[i].String()) < strings.ToLower(tsks[j].String())
+		})
+
+		curDay := startDate.AddDate(0, 0, day)
+		curDayStr := fmt.Sprintf("\n%s", curDay.Format(printTimeFormat))
+		if day == 0 {
+			curDayStr += " (today)"
+		}
+		fmt.Println(curDayStr)
+
+		for _, tsk := range tsks {
+			fmt.Printf("\t-%s\n", tsk)
+		}
+	}
+}
+
+func parseArgs(args *cmdArgs, argsIn []string) error {
+	if len(argsIn) < 2 {
+		return args.parseArgs([]string{"--help"})
+	}
+	err := args.parseArgs(argsIn[1:])
+	if err != nil {
+		return err
+	}
+	if len(args.weeklySources)+len(args.monthlySources)+len(args.multiDateSources) == 0 {
+		return errors.New("no source files provided")
+	}
+	return nil
+}
+
+// stringSliceArg is a flag that accumulates strings
+type stringSliceArg []string
+
+func (s *stringSliceArg) String() string {
+	return strings.Join(*s, " ")
+}
+
+func (s *stringSliceArg) Set(val string) error {
+	*s = append(*s, val)
+	return nil
+}
+
+func setUsage(fs *flag.FlagSet) {
+	fs.Usage = func() {
+		fmt.Printf("%s displays upcoming scheduled tasks\n\n", name)
+		fmt.Print("Usage:\n")
+		fmt.Printf("  %s [flags]\n\n", name)
+		fmt.Printf("Flags:\n")
+		fs.PrintDefaults()
+	}
+}
--- main.go
+++ main.go
@@ -1,98 +1,17 @@
 package main
 
 import (
-	"flag"
-	"fmt"
 	"log"
-	"sort"
-	"strings"
-	"time"
+	"os"
 
-	"github.com/dkaslovsky/calendar-tasks/pkg/tasks"
+	"github.com/dkaslovsky/calendar-tasks/cmd"
 )
 
 func main() {
-	date := fixDate(time.Now())
+	log.SetFlags(0)
 
-	args := cmdArgs{}
-	args.attachArgs()
-
-	taskChan := make(chan tasks.Task, 1000) // buffer size is large enough for a reasonable amount of tasks
-	done := make(chan struct{})
-
-	loader := tasks.NewLoader(taskChan, done)
-	processor := tasks.NewProcessor(date, args.days, taskChan, done)
-
-	loader.AddWeeklySource(args.weeklySources...)
-	loader.AddMonthlySource(args.monthlySources...)
-	loader.AddMultiDateSource(args.multiDateSources...)
-
-	err := run(loader, processor)
+	err := cmd.Run(os.Args)
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	printTasks(processor, args.days, date)
-}
-
-func run(loader *tasks.Loader, processor *tasks.Processor) error {
-	// start the processor and wait on it to finish before returning
-	processor.Start()
-	defer processor.Wait()
-
-	// start the loader and return any errors
-	return loader.Start()
-}
-
-func printTasks(processor *tasks.Processor, numDays int, startDate time.Time) {
-	for day := 0; day <= numDays; day++ {
-		tsks, ok := processor.GetTasks(day)
-		if !ok {
-			continue
-		}
-
-		// sort for consistent ordering
-		sort.Slice(tsks, func(i, j int) bool {
-			return strings.ToLower(tsks[i].String()) < strings.ToLower(tsks[j].String())
-		})
-
-		curDay := startDate.AddDate(0, 0, day)
-		fmt.Printf("\n%s\n", curDay.Format("[Mon] Jan 2 2006"))
-		for _, tsk := range tsks {
-			fmt.Printf("\t-%s\n", tsk)
-		}
-	}
-}
-
-// fixDate returns a time.Time object matching the year, month, day (and location) of the argument
-// and sets the hour to the middle of the day to avoid any boundary cases that can occur with
-// e.g., daylight savings
-func fixDate(now time.Time) time.Time {
-	return time.Date(now.Year(), now.Month(), now.Day(), 12, 0, 0, 0, now.Location())
-}
-
-type cmdArgs struct {
-	days             int
-	weeklySources    stringSliceArg
-	monthlySources   stringSliceArg
-	multiDateSources stringSliceArg
-}
-
-func (args *cmdArgs) attachArgs() {
-	flag.IntVar(&args.days, "days", 0, "days ahead to get tasks")
-	flag.Var(&args.weeklySources, "weekly", "weekly task source file")
-	flag.Var(&args.monthlySources, "monthly", "monthly task source file")
-	flag.Var(&args.multiDateSources, "multi", "multiDate task source file")
-	flag.Parse()
-}
-
-type stringSliceArg []string
-
-func (s *stringSliceArg) String() string {
-	return strings.Join(*s, " ")
-}
-
-func (s *stringSliceArg) Set(val string) error {
-	*s = append(*s, val)
-	return nil
 }
