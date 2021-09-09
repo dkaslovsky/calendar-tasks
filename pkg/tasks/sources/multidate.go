9ad0ab44b27473d88ac4a200d8fbed18ead87ffa
dkaslovsky <dkaslovsky@gmail.com>
2021-09-08 20:22:08 -0600

    convert multidate to single date

--- pkg/tasks/sources/multidate.go
+++ pkg/tasks/sources/multidate.go
@@ -10,16 +10,10 @@ import (
 	"github.com/dkaslovsky/calendar-tasks/pkg/calendar"
 )
 
-const monthDelim = "/"
-
-type date struct {
-	month time.Month
-	day   int
-}
-
 // MultiDate represents a task with multiple dates
 type MultiDate struct {
-	dates []*date
+	month time.Month
+	day   int
 	text  string
 }
 
@@ -33,25 +27,14 @@ func NewMultiDate(raw *RawTask) (*MultiDate, error) {
 	if err != nil {
 		return &MultiDate{}, fmt.Errorf("could not parse date: %v", err)
 	}
-	months := strings.Split(dateParts[0], monthDelim)
-	if len(months) == 0 || len(months) > 12 {
+	month, err := calendar.ParseMonth(dateParts[0])
+	if err != nil {
 		return &MultiDate{}, fmt.Errorf("invalid multiple date [%s]", raw.Date)
 	}
 
-	dates := []*date{}
-	for _, m := range months {
-		month, err := calendar.ParseMonth(m)
-		if err != nil {
-			return &MultiDate{}, fmt.Errorf("invalid multiple date [%s]", raw.Date)
-		}
-		dates = append(dates, &date{
-			month: month,
-			day:   int(day),
-		})
-	}
-
 	m := &MultiDate{
-		dates: dates,
+		month: month,
+		day:   int(day),
 		text:  raw.Text,
 	}
 	return m, nil
@@ -63,8 +46,20 @@ func (m *MultiDate) DaysFrom(t time.Time) int {
 	nowMonth := t.Month()
 	nowLoc := t.Location()
 
+	type date struct {
+		month time.Month
+		day   int
+	}
+
+	dates := []*date{
+		{
+			month: m.month,
+			day:   m.day,
+		},
+	}
+
 	curDiff := 10e8 // any large value > 366 will work
-	for _, date := range m.dates {
+	for _, date := range dates {
 		curT := time.Date(nowYear, date.month, date.day, 0, 0, 0, 0, nowLoc)
 		diff := curT.Sub(t).Hours() / 24
 		if diff < 0 {
--- pkg/tasks/sources/multidate_test.go
+++ pkg/tasks/sources/multidate_test.go
@@ -1,33 +1,9 @@
 package sources
 
 import (
-	"sort"
 	"testing"
 	"time"
 )
-
-func assertEqualDateSlice(t *testing.T, expected, actual []*date) {
-	if len(expected) != len(actual) {
-		t.Fatalf("number of result dates %d not equal to number of expected dates %d", len(actual), len(expected))
-	}
-
-	actualDates := actual
-	expectedDates := expected
-	sort.Slice(actualDates, func(i, j int) bool {
-		return actualDates[i].month > actualDates[j].month
-	})
-	sort.Slice(expectedDates, func(i, j int) bool {
-		return expectedDates[i].month > expectedDates[j].month
-	})
-
-	for i := 0; i < len(expectedDates); i++ {
-		a := actualDates[i]
-		e := expectedDates[i]
-		if a.month != e.month || a.day != e.day {
-			t.Fatalf("unexpected result date %v", a)
-		}
-	}
-}
 
 func TestMultiDateDaysFrom(t *testing.T) {
 	tests := map[string]struct {
@@ -35,194 +11,98 @@ func TestMultiDateDaysFrom(t *testing.T) {
 		now      time.Time
 		expected int
 	}{
-		"same day with single date": {
+		"same day": {
 			r: &MultiDate{
-				dates: []*date{
-					{
-						month: time.August,
-						day:   6,
-					},
-				},
+				month: time.August,
+				day:   6,
 			},
 			now:      time.Date(2021, time.August, 6, 0, 0, 0, 0, time.UTC),
 			expected: 0,
 		},
-		"same day with multiple dates": {
+		"next day": {
 			r: &MultiDate{
-				dates: []*date{
-					{
-						month: time.August,
-						day:   6,
-					},
-					{
-						month: time.November,
-						day:   6,
-					},
-				},
-			},
-			now:      time.Date(2021, time.August, 6, 0, 0, 0, 0, time.UTC),
-			expected: 0,
-		},
-		"next day with single date": {
-			r: &MultiDate{
-				dates: []*date{
-					{
-						month: time.August,
-						day:   7,
-					},
-				},
-			},
-			now:      time.Date(2021, time.August, 6, 0, 0, 0, 0, time.UTC),
-			expected: 1,
-		},
-		"next day with multiple dates": {
-			r: &MultiDate{
-				dates: []*date{
-					{
-						month: time.August,
-						day:   7,
-					},
-					{
-						month: time.November,
-						day:   7,
-					},
-				},
+				month: time.August,
+				day:   7,
 			},
 			now:      time.Date(2021, time.August, 6, 0, 0, 0, 0, time.UTC),
 			expected: 1,
 		},
 		"previous day": {
 			r: &MultiDate{
-				dates: []*date{
-					{
-						month: time.August,
-						day:   5,
-					},
-				},
+				month: time.August,
+				day:   5,
 			},
 			now:      time.Date(2021, time.August, 6, 0, 0, 0, 0, time.UTC),
 			expected: 364,
 		},
 		"previous day including leap year starting past February": {
 			r: &MultiDate{
-				dates: []*date{
-					{
-						month: time.August,
-						day:   5,
-					},
-				},
+				month: time.August,
+				day:   5,
 			},
 			now:      time.Date(2023, time.August, 6, 0, 0, 0, 0, time.UTC),
 			expected: 365,
 		},
 		"previous day including leap year starting before February": {
 			r: &MultiDate{
-				dates: []*date{
-					{
-						month: time.January,
-						day:   5,
-					},
-				},
+				month: time.January,
+				day:   5,
 			},
 			now:      time.Date(2024, time.January, 6, 0, 0, 0, 0, time.UTC),
 			expected: 365,
 		},
 		"previous day including leap year starting in February": {
 			r: &MultiDate{
-				dates: []*date{
-					{
-						month: time.February,
-						day:   5,
-					},
-				},
+				month: time.February,
+				day:   5,
 			},
 			now:      time.Date(2024, time.February, 6, 0, 0, 0, 0, time.UTC),
 			expected: 365,
 		},
 		"previous day including leap year starting on February 28": {
 			r: &MultiDate{
-				dates: []*date{
-					{
-						month: time.February,
-						day:   27,
-					},
-				},
+				month: time.February,
+				day:   27,
 			},
 			now:      time.Date(2024, time.February, 28, 0, 0, 0, 0, time.UTC),
 			expected: 365,
 		},
 		"previous day including leap year starting on February 29": {
 			r: &MultiDate{
-				dates: []*date{
-					{
-						month: time.February,
-						day:   28,
-					},
-				},
+				month: time.February,
+				day:   28,
 			},
 			now:      time.Date(2024, time.February, 29, 0, 0, 0, 0, time.UTC),
 			expected: 365,
 		},
-		"next month single date": {
+		"next month": {
 			r: &MultiDate{
-				dates: []*date{
-					{
-						month: time.September,
-						day:   6,
-					},
-				},
-			},
-			now:      time.Date(2021, time.August, 6, 0, 0, 0, 0, time.UTC),
-			expected: 31,
-		},
-		"next month multiple dates": {
-			r: &MultiDate{
-				dates: []*date{
-					{
-						month: time.September,
-						day:   6,
-					},
-					{
-						month: time.November,
-						day:   6,
-					},
-				},
+				month: time.September,
+				day:   6,
 			},
 			now:      time.Date(2021, time.August, 6, 0, 0, 0, 0, time.UTC),
 			expected: 31,
 		},
 		"within 24 hours": {
 			r: &MultiDate{
-				dates: []*date{
-					{
-						month: time.August,
-						day:   15,
-					},
-				},
+				month: time.August,
+				day:   15,
 			},
 			now:      time.Date(2021, time.August, 14, 18, 0, 0, 0, time.UTC),
 			expected: 1,
 		},
 		"one second away from today": {
 			r: &MultiDate{
-				dates: []*date{
-					{
-						month: time.August,
-						day:   15,
-					},
-				},
+				month: time.August,
+				day:   15,
 			},
 			now:      time.Date(2021, time.August, 14, 23, 59, 59, 0, time.UTC),
 			expected: 1,
 		},
 		"exactly same time": {
 			r: &MultiDate{
-				dates: []*date{
-					{
-						month: time.August,
-						day:   15,
-					},
-				},
+				month: time.August,
+				day:   15,
 			},
 			now:      time.Date(2021, time.August, 15, 0, 0, 0, 0, time.UTC),
 			expected: 0,
@@ -242,60 +122,19 @@ func TestMultiDateDaysFrom(t *testing.T) {
 
 func TestNewMultiDate(t *testing.T) {
 	tests := map[string]struct {
-		raw           *RawTask
-		expectedDates []*date
-		expectedText  string
+		raw      *RawTask
+		expected *MultiDate
 	}{
 		"single month": {
 			raw: &RawTask{
-				Date: "april 1",
+				Date: "april 17",
 				Text: "foo bar woo",
 			},
-			expectedDates: []*date{
-				{
-					month: time.April,
-					day:   1,
-				},
+			expected: &MultiDate{
+				month: time.April,
+				day:   17,
+				text:  "foo bar woo",
 			},
-			expectedText: "foo bar woo",
-		},
-		"multiple months": {
-			raw: &RawTask{
-				Date: "april/may 12",
-				Text: "foo bar woo",
-			},
-			expectedDates: []*date{
-				{
-					month: time.April,
-					day:   12,
-				},
-				{
-					month: time.May,
-					day:   12,
-				},
-			},
-			expectedText: "foo bar woo",
-		},
-		"multiple months unordered": {
-			raw: &RawTask{
-				Date: "april/may/january 15",
-				Text: "foo bar woo",
-			},
-			expectedDates: []*date{
-				{
-					month: time.April,
-					day:   15,
-				},
-				{
-					month: time.May,
-					day:   15,
-				},
-				{
-					month: time.January,
-					day:   15,
-				},
-			},
-			expectedText: "foo bar woo",
 		},
 	}
 
@@ -306,10 +145,15 @@ func TestNewMultiDate(t *testing.T) {
 			if err != nil {
 				t.Fatalf("unexpected non-nil error: %v", err)
 			}
-			if result.text != test.expectedText {
-				t.Fatalf("result text '%s' not equal to expected text '%s'", result.text, test.expectedText)
+			if result.text != test.expected.text {
+				t.Fatalf("result text '%s' not equal to expected text '%s'", result.text, test.expected.text)
 			}
-			assertEqualDateSlice(t, test.expectedDates, result.dates)
+			if result.month != test.expected.month {
+				t.Fatalf("result month '%s' not equal to expected month '%s'", result.month, test.expected.month)
+			}
+			if result.day != test.expected.day {
+				t.Fatalf("result day '%d' not equal to expected day '%d'", result.day, test.expected.day)
+			}
 		})
 	}
 
@@ -327,24 +171,14 @@ func TestNewMultiDateError(t *testing.T) {
 				Date: "xxx",
 			},
 		},
-		"invalid second month": {
-			raw: &RawTask{
-				Date: "april/xxx",
-			},
-		},
-		"empty second month": {
-			raw: &RawTask{
-				Date: "april/",
-			},
-		},
 		"month without day": {
 			raw: &RawTask{
-				Date: "april/may",
+				Date: "april",
 			},
 		},
 		"month with invalid day": {
 			raw: &RawTask{
-				Date: "april/may xxx",
+				Date: "april 1xxx",
 			},
 		},
 	}
