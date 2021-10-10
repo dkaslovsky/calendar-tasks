cf45712d9e644adf8a3b8400e8e399bdf8e463f4
dkaslovsky <dkaslovsky@gmail.com>
2021-10-10 13:48:52 -0600

    app name and version in main

--- main.go
+++ main.go
@@ -7,10 +7,15 @@ import (
 	"github.com/dkaslovsky/calendar-tasks/cmd"
 )
 
+const (
+	name    = "calendar-tasks" // app name
+	version = "0.1.1"          // hard-code version for now
+)
+
 func main() {
 	log.SetFlags(0)
 
-	err := cmd.Run(os.Args)
+	err := cmd.Run(name, version, os.Args)
 	if err != nil {
 		log.Fatal(err)
 	}
